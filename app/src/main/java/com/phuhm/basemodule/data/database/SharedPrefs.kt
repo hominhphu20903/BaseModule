8aacd71de208184afaf34c244f3bf55509020ad9
Phu <hominhphu20903@gmail.com>
2025-02-05 10:23:58 +0700

    Update SharedPrefs.kt

--- app/src/main/java/com/phuhm/basemodule/data/database/SharedPrefs.kt
+++ app/src/main/java/com/phuhm/basemodule/data/database/SharedPrefs.kt
@@ -1,27 +1,18 @@
 package com.phuhm.basemodule.data.database
 
-import android.annotation.SuppressLint
 import android.content.Context
 import com.phuhm.basemodule.shared.Constants
 
-class SharedPrefs(context: Context) {
-    private val sharePrefs = context.getSharedPreferences(Constants.DB_NAME, Context.MODE_PRIVATE)
-    private val editor = sharePrefs.edit()
+object SharedPrefs {
+    private lateinit var sharePrefs: android.content.SharedPreferences
 
-    companion object {
-        @SuppressLint("StaticFieldLeak")
-        private var INSTANCE: SharedPrefs? = null
-        fun getInstance(context: Context): SharedPrefs {
-            if (INSTANCE == null) {
-                INSTANCE = SharedPrefs(context)
-            }
-            return INSTANCE!!
-        }
+    fun init(context: Context) {
+        sharePrefs = context.getSharedPreferences(Constants.DB_NAME, Context.MODE_PRIVATE)
     }
 
-    var isOpenedSplash
+    var isOpenedSplash: Boolean
         get() = sharePrefs.getBoolean(Constants.IS_OPENED_SPLASH, false)
         set(value) {
-            editor.putBoolean(Constants.IS_OPENED_SPLASH, value).apply()
+            sharePrefs.edit().putBoolean(Constants.IS_OPENED_SPLASH, value).apply()
         }
 }
